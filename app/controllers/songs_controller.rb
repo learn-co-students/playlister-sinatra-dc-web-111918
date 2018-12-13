b426d5133f8412980dc66356598b22a90214b3f2
rnovick13 <rnovick13@gmail.com>
2018-12-13 15:42:11 -0500

    latest changes as of 3:40

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,16 @@ class SongsController < ApplicationController
   end
 
   get "/songs/new" do
+    @song = Song.new
     erb :new
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    redirect "/songs/#{@song.name.slug}"
   end
 
   get "/songs/:slug" do
